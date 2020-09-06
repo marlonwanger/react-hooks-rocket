674c9e82f19803f9d397422034419039bce00e47
Marlon Wanger <marlonsoftwares@gmail.com>
2020-09-06 18:20:31 -0400

    finalizando exemplo

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import React, { useState, useEffect } from 'react';
 function App() {
   //    valor do state, seta o state          valor inicial
   const [repositories, setRepositories] = useState([]);
+  const [location, setLocation] = useState({});
 
   useEffect(() => {
     async function getGithubData() {
@@ -14,15 +15,38 @@ function App() {
       const data = await response.json();
       setRepositories(data);
     }
-
     getGithubData();
+
+    const watchId = navigator.geolocation.watchPosition(handlePositionReceived);
+
+    // toda vez que um effect tiver return é executado como 'willUnmount'
+    return () => navigator.geolocation.clearWatch(watchId);
   }, []); /* no segundo parametro e passo variaveis (comparaçoes) indicando quando esse efect
   vai ser executado vazio, vai ser executado apenas 1 vez */
+
+  // esse aqui pode ser considerado um coponentDidUpdate
+  useEffect(() => {
+    //filter espera um bool e favorite é true or false
+    const filtered = repositories.filter((repo) => repo.favorite);
+
+    document.title = `Você tem ${filtered.length} favoritos`;
+  }, [repositories]); // só será executado quando repositories mudar.
+
+  function handlePositionReceived({ coords }) {
+    console.log(coords);
+
+    const { latitude, longitude } = coords;
+
+    setLocation({
+      latitude,
+      longitude,
+    });
+  }
 
   function handleFavorite(id) {
     const newRepositories = repositories.map((repo) => {
       // retorna o repositorio inteiro + a opcao de favorite
-      return repo.id === id ? { ...repo, favorite: true } : repo;
+      return repo.id === id ? { ...repo, favorite: !repo.favorite } : repo;
     });
 
     setRepositories(newRepositories);
@@ -31,7 +55,6 @@ function App() {
   return (
     <div className="App">
       <h2>Repositories List</h2>
-
       <ul>
         {repositories.map((repo) => (
           <li key={repo.id}>
@@ -43,6 +66,9 @@ function App() {
           </li>
         ))}
       </ul>
+      <h3>Coodinates</h3>
+      Latitude: {location.latitude} <br />
+      Longitude: {location.longitude}
     </div>
   );
 }
